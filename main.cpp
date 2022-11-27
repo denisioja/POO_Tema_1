3bb89a3458cd0649151dd3836fb0471506a10a92
Denis <denisioja2002@gmail.com>
2022-11-28 00:48:18 +0200

    Modificat

--- main.cpp
+++ main.cpp
@@ -3,57 +3,84 @@
 #include <vector>
 #include <algorithm>
 
-class Copaci {
+class Planta {
 private:
     std::string specie;
-    int numar;
+    int an_plantare;
 public:
-    Copaci() = default;
+    Planta() = default;
 
-    Copaci(const std::string &specie_, int numar_);
+    Planta(const std::string &specie_, int an_plantare_);
 
-    Copaci(const Copaci &copaci);
+    Planta(const Planta &planta);
 
-    Copaci& operator=(const Copaci &copaci) {
-        specie = copaci.specie;
-        numar = copaci.numar;
+    friend std::ostream & operator <<(std::ostream &out, Planta &planta);
+
+    Planta& operator = (const Planta &planta){
+        specie = planta.specie;
+        an_plantare = planta.an_plantare;
         return *this;
-    };
-
-//    int getNumar() const {
-//        return numar;
-//    }
-
-    void afiscop(){
-        std::cout << specie << " " << numar << "\n";
     }
 
-    static Copaci* sumaC(const std::vector<Copaci> &copaci) {
-        int suma=0;
-        for(int i = 0; i < copaci.size(); i++)
-            suma = suma + copaci[i].numar;
-        Copaci* total = new Copaci("Totalitatea copacilor", suma);
-        return total;
-    }
-
-    ~Copaci() = default;
-
-    friend std::ostream & operator <<(std::ostream &out, Copaci &copaci);
+    ~Planta() = default;
 };
 
-Copaci::Copaci(const std::string &specie_, int numar_) {
+Planta::Planta(const std::string &specie_, int an_plantare_) {
     this->specie = specie_;
-    this->numar = numar_;
+    this->an_plantare = an_plantare_;
 }
 
-Copaci::Copaci(const Copaci &copaci) {
-    this->specie = copaci.specie;
-    this->numar = copaci.numar;
+Planta::Planta(const Planta &planta) {
+    this->specie = planta.specie;
+    this->an_plantare = planta.an_plantare;
 }
 
-std::ostream & operator <<(std::ostream &out, Copaci &copaci) {
-    out <<  "Specie copac: " << copaci.specie << ", numarul de bucati: " << copaci.numar << "\n";
+std::ostream & operator <<(std::ostream &out, Planta &planta){
+    out << "Specie planta: " << planta.specie << ", plantata in anul: " << planta.an_plantare << "\n";
     return out;
+}
+
+class Vegetatie {
+private:
+    std::vector<Planta> plante;
+    std::string tip; //copac, tufis, floare
+public:
+    Vegetatie() = default;
+
+    Vegetatie(const std::vector <Planta> &plante_, const std::string &tip_);
+
+    Vegetatie(const Vegetatie &vegetatie);
+
+    Vegetatie& operator= (const Vegetatie &vegetatie){
+        plante = vegetatie.plante;
+        tip = vegetatie.tip;
+        return *this;
+    }
+
+    friend std::ostream & operator <<(std::ostream &out, Vegetatie &vegetatie);
+
+    void adugare_planta(Planta planta){
+        plante.emplace_back(planta);
+    }
+
+    ~Vegetatie() = default;
+};
+
+Vegetatie::Vegetatie(const std::vector<Planta> &plante_, const std::string &tip_) {
+    this->plante = plante_;
+    this->tip = tip_;
+}
+
+Vegetatie::Vegetatie(const Vegetatie &vegetatie){
+    this->plante = vegetatie.plante;
+    this->tip = vegetatie.tip;
+}
+
+std::ostream & operator <<(std::ostream &out, Vegetatie &vegetatie){
+    out << "Tip: " << vegetatie.tip << "\n";
+    for(auto& planta : vegetatie.plante){
+        out << planta;
+    }
 }
 
 class Lac {
@@ -152,18 +179,18 @@ class Parc {
 private:
     std::string nume;
     float suprafata;
-    std::vector<Copaci> copaci;
+    Vegetatie vegetatie;
     std::vector<Banci> banci;
     Lac lac;
 public:
-    Parc(const std::string &nume_, float suprafata_, const std::vector<Copaci> &copaci_, const std::vector<Banci> &banci_, const Lac &lac_);
+    Parc(const std::string &nume_, float suprafata_, const Vegetatie vegetatie_, const std::vector<Banci> &banci_, const Lac &lac_);
 
     Parc(const Parc &parc);
 
     Parc& operator=(const Parc &parc) {
         nume = parc.nume;
         suprafata = parc.suprafata;
-        copaci = parc.copaci;
+        vegetatie = parc.vegetatie;
         banci = parc.banci;
         lac = parc.lac;
         return *this;
@@ -178,12 +205,13 @@ public:
     ~Parc() = default;
 
     friend std::ostream & operator <<(std::ostream &out, Parc &parc);
+
 };
 
-Parc::Parc(const std::string &nume_, float suprafata_, const std::vector<Copaci> &copaci_, const std::vector<Banci> &banci_, const Lac &lac_) {
+Parc::Parc(const std::string &nume_, float suprafata_, const Vegetatie vegetatie_, const std::vector<Banci> &banci_, const Lac &lac_) {
     this->nume = nume_;
     this->suprafata = suprafata_;
-    this->copaci = copaci_;
+    this->vegetatie = vegetatie_;
     this->banci = banci_;
     this->lac = lac_;
 }
@@ -191,32 +219,82 @@ Parc::Parc(const std::string &nume_, float suprafata_, const std::vector<Copaci>
 Parc::Parc(const Parc &parc) {
     this->nume = parc.nume;
     this->suprafata = parc.suprafata;
-    this->copaci = parc.copaci;
+    this->vegetatie = parc.vegetatie;
     this->lac = parc.lac;
     this->banci = parc.banci;
 }
 
 std::ostream & operator <<(std::ostream &out, Parc &parc) {
+    out << "Date parc: \n";
     out << "Numele parcului: " << parc.nume << ", avand suprafata de: " << parc.suprafata << " m patrati\n";
-    out << "Copaci:\n";
-    for(int i = 0; i < parc.copaci.size(); i++)
-        out << parc.copaci[i];
-    out << "Lacul:\n";
-    out << parc.lac;
-    out << "Bancile:\n";
-    for(int i = 0; i < parc.banci.size(); i++)
-        out << parc.banci[i];
+    std::string input;
+    int cont = 1;
+    int info;
+    std::string veg = "Vegetatie";
+    std::string lac = "Lac";
+    std::string bnc = "Banci";
+    std::string da = "DA";
+    std::string nu = "NU";
+
+    while(cont!=0){
+        out << "Selectati o categorie de informatii:\n";
+        out << "Vegetatie / Lac / Banci\n";
+        std::cin >> input;
+
+        if(input.compare(veg) == 0)
+            info=1;
+        if(input.compare(lac)==0)
+            info=2;
+        if(input.compare(bnc)==0)
+            info=3;
+
+        switch (info) {
+            case 1:
+                out << "Vegetatia este formata din:\n";
+                out << parc.vegetatie;
+                break;
+            case 2:
+                out << "Lacul:\n";
+                out << parc.lac;
+                break;
+            case 3:
+                out << "Bancile:\n";
+                for (int i = 0; i < parc.banci.size(); i++)
+                    out << parc.banci[i];
+                break;
+        }
+        out << "Doriti sa mai aflati informatii? DA/NU\n";
+        std::cin >> input;
+        if(input.compare(da)==0)
+            cont = 1;
+        if(input.compare(nu)==0)
+            cont = 0;
+    }
     return out;
 }
 
 int main(){
-    std::vector<Copaci> copaci;
-    copaci.reserve(100);
-    copaci.emplace_back("Tei", 15);
-    copaci.emplace_back("Stejar", 20);
-    copaci.emplace_back("Salcam", 17);
+    Planta planta1, planta2, planta3, planta4, planta5;
+    planta1 = {"Tei", 1950};
+    planta2 = {"Trandafir", 2005};
+    planta3 = {"Stejar", 1989};
+    planta4 = {"Salcam", 1999};
+    planta5 = {"Tuia", 2009};
+
+    Vegetatie copaci({planta1}, "Copaci");
+    copaci.adugare_planta(planta3);
+    copaci.adugare_planta(planta4);
+
+    Vegetatie flori({planta2}, "Flori"), tufe({planta5}, "Tufe");
+
+//    std::cout << copaci;
+//    std::cout << flori;
+//    std::cout << tufe;
+//    std::cout << "\n";
 
     Lac lac(123, "Natural");
+//    std::cout << lac;
+//    std::cout << "\n";
 
     std::vector<Banci> banci;
     banci.reserve(100);
@@ -228,12 +306,13 @@ int main(){
     Parc parc("IOR", 234, copaci, banci, lac);
     std::cout << parc;
 
-    Copaci* sumC = Copaci::sumaC(copaci);
-    sumC->afiscop();
-    delete sumC;
+//    Copaci* sumC = Copaci::sumaC(copaci);
+//    sumC->afiscop();
+//    delete sumC;
 
+    std::cout << "\n";
     Banci* sumB = Banci::sumaB(banci);
-    sumB->afisbnc();
+//    sumB->afisbnc();
     delete sumB;
 
     //Parc dim = dimensiune(parc);
