5d84f599ed9cc0320dea3bec3f92f52177aa7e09
Denis <denisioja2002@gmail.com>
2022-12-05 01:18:42 +0200

    Modificat din nou

--- main.cpp
+++ main.cpp
@@ -16,6 +16,10 @@ public:
 
     friend std::ostream & operator <<(std::ostream &out, Planta &planta);
 
+    int get_an() const {
+        return an_plantare;
+    }
+
     Planta& operator = (const Planta &planta){
         specie = planta.specie;
         an_plantare = planta.an_plantare;
@@ -23,6 +27,9 @@ public:
     }
 
     ~Planta() = default;
+//    ~Planta(){
+//        std::cout << "Apelare destructor";
+//    };
 };
 
 Planta::Planta(const std::string &specie_, int an_plantare_) {
@@ -63,6 +70,17 @@ public:
         plante.emplace_back(planta);
     }
 
+//    std::vector<Planta> Vegetatie::cauta_plante(int an); //_start, int an_sfarsit);
+    void cauta_plante(int an){
+        int i;
+        std::cout << "Pana in anul " << an << " au fost plantate:\n";
+        for(i=0;i<plante.size();i++){
+            if(plante[i].get_an()<=an){
+                std::cout << plante[i] << "\n";
+            }
+        }
+    };
+
     ~Vegetatie() = default;
 };
 
@@ -101,9 +119,9 @@ public:
         return *this;
     }
 
-    float getSuprafata() const {
-        return this->suprafata;
-    }
+//    float getSuprafata() const {
+//        return this->suprafata;
+//    }
 
     ~Lac() = default;
 
@@ -129,49 +147,83 @@ class Banci {
 private:
     int numar;
     std::string culoare;
+    std::string stare; // foarte buna, buna, medie, proasta
+    int pret;
 public:
 
     Banci() = default;
 
-    Banci(int numar_, const std::string &culoare_);
+    Banci(int numar_, const std::string &culoare_, const std::string &stare_, int pret_);
 
     Banci(const Banci &banci);
 
     Banci& operator=(const Banci &banci) {
         numar = banci.numar;
         culoare = banci.culoare;
+        stare = banci.stare;
+        pret = banci.pret;
         return *this;
     }
 
     void afisbnc(){
-        std::cout << culoare << " " << numar << "\n";
+        std::cout << culoare << " " << numar << " " << stare << " " << pret <<"\n";
     }
 
-    static Banci* sumaB(const std::vector<Banci> &banci) {
-        int suma=0;
-        for(int i = 0; i < banci.size(); i++)
-            suma = suma + banci[i].numar;
-        Banci* total = new Banci(suma, "Totalitatea bancilor");
-        return total;
+//    static Banci* sumaB(const std::vector<Banci> &banci) {
+//        int suma=0;
+//        for(int i = 0; i < banci.size(); i++)
+//            suma = suma + banci[i].numar;
+//        Banci* total = new Banci(suma, "Totalitatea bancilor");
+//        return total;
+//    }
+
+    std::string get_stare() const {
+        return stare;
     }
+
+    std::string get_culoare() const {
+        return culoare;
+    }
+
+    int get_pret() const {
+        return pret;
+    }
+
+    int get_numar() const {
+        return numar;
+    }
+
+    void crestere_nr(int nr){
+        numar = numar + nr;
+    }
+
+    void scadere_nr(int nr){
+        numar = numar - nr;
+    }
+
+    void set_Nr(int numar_) {numar = numar_;}
 
     ~Banci() = default;
 
     friend std::ostream & operator <<(std::ostream &out, Banci &banci);
 };
 
-Banci::Banci(int numar_, const std::string &culoare_) {
+Banci::Banci(int numar_, const std::string &culoare_, const std::string &stare_, int pret_) {
     this->numar = numar_;
     this->culoare = culoare_;
+    this->stare = stare_;
+    this->pret = pret_;
 }
 
 Banci::Banci(const Banci &banci) {
     this->numar = banci.numar;
     this->culoare = banci.culoare;
+    this->stare = banci.stare;
+    this->pret = banci.pret;
 }
 
 std::ostream & operator <<(std::ostream &out, Banci &banci) {
-    out << "Culoare banci: " << banci.culoare << ", numarul de bucati: " << banci.numar << "\n";
+    out << "Culoare banci: " << banci.culoare << ", numarul de bucati: " << banci.numar << ", fiind in starea: " << banci.stare << ", si avand pretul de: " << banci.pret << " lei\n";
     return out;
 }
 
@@ -196,17 +248,24 @@ public:
         return *this;
     }
 
-    friend Parc dimensiune(const Parc &parc) {
-        if(parc.suprafata>parc.lac.getSuprafata())
-            std::cout << "Suprafata parcului este mai mare decat cea a lacului\n";
-        else std::cout << "Suprafata lacului este mai mare decat cea a parcului\n";
-    }
+//    friend Parc dimensiune(const Parc &parc) {
+//        if(parc.suprafata>parc.lac.getSuprafata())
+//            std::cout << "Suprafata parcului este mai mare decat cea a lacului\n";
+//        else std::cout << "Suprafata lacului este mai mare decat cea a parcului\n";
+//    }
+
+//    void Parc::achizitie_banci(int buget); // determină tipul de bănci în starea cea mai bună și înlocuiește o parte din celelalte bănci cu acest tip de bănci în limita bugetului
+    void achizitie_banci(int buget);
 
     ~Parc() = default;
 
     friend std::ostream & operator <<(std::ostream &out, Parc &parc);
 
 };
+
+void get_input(std::string &input) {
+    std::cin >> input;
+}
 
 Parc::Parc(const std::string &nume_, float suprafata_, const Vegetatie vegetatie_, const std::vector<Banci> &banci_, const Lac &lac_) {
     this->nume = nume_;
@@ -224,6 +283,64 @@ Parc::Parc(const Parc &parc) {
     this->banci = parc.banci;
 }
 
+void Parc::achizitie_banci(int buget) {
+    int i, pret_fb, fb;
+    std::string best;
+    std::string state = "foarte buna";
+    for(i=0;i<banci.size();i++){
+        if(state.compare(banci[i].get_stare())==0){
+            best = banci[i].get_culoare();
+            pret_fb = banci[i].get_pret();
+            fb = i;
+            break;
+        }
+    }
+    i=1;
+    int j;
+    int nr_add = 0;
+    while(buget>pret_fb && i < 4){
+        switch (i) {
+            case 1:
+                state = "proasta";
+                break;
+            case 2:
+                state = "medie";
+                break;
+            case 3:
+                state = "buna";
+                break;
+        }
+        for(j=0;j<banci.size();j++){
+            if(state.compare(banci[j].get_stare())==0){
+                if(banci[j].get_numar()*pret_fb <= buget){
+                    //banci[fb].crestere_nr(banci[j].get_numar()); de ce nu imi actualizeaza decat prima data?
+                    nr_add = nr_add + banci[j].get_numar();
+                    //std::cout << banci[fb].get_numar() << " nr banci fb\n";
+                    buget = buget - banci[j].get_numar() * pret_fb;
+                    banci.erase(banci.begin()+j);
+                    break;
+                }
+                else{
+                    int nr_banci_bug = buget/pret_fb;
+                    //std::cout << buget << " " << pret_fb << " " << nr_banci_bug << " - nr banci bug\n";
+                    banci[j].scadere_nr(nr_banci_bug);
+                    //banci[fb].crestere_nr(nr_banci_bug); same;
+                    nr_add = nr_add + nr_banci_bug;
+                    buget = buget - banci[fb].get_pret() * nr_banci_bug;
+                    //std::cout << buget << " bugetul\n";
+                    break;
+                }
+            }
+            if(buget<pret_fb)
+                break;
+        }
+        i++;
+    }
+    //std::cout << nr_add << " nr de adunat\n";
+    banci[fb].crestere_nr(nr_add);
+    //banci[fb].set_Nr(nr_add+banci[fb].get_numar());
+}
+
 std::ostream & operator <<(std::ostream &out, Parc &parc) {
     out << "Date parc: \n";
     out << "Numele parcului: " << parc.nume << ", avand suprafata de: " << parc.suprafata << " m patrati\n";
@@ -239,7 +356,7 @@ std::ostream & operator <<(std::ostream &out, Parc &parc) {
     while(cont!=0){
         out << "Selectati o categorie de informatii:\n";
         out << "Vegetatie / Lac / Banci\n";
-        std::cin >> input;
+        get_input(input);
 
         if(input.compare(veg) == 0)
             info=1;
@@ -264,7 +381,7 @@ std::ostream & operator <<(std::ostream &out, Parc &parc) {
                 break;
         }
         out << "Doriti sa mai aflati informatii? DA/NU\n";
-        std::cin >> input;
+        get_input(input);
         if(input.compare(da)==0)
             cont = 1;
         if(input.compare(nu)==0)
@@ -298,22 +415,27 @@ int main(){
 
     std::vector<Banci> banci;
     banci.reserve(100);
-    banci.emplace_back(12, "Rosii");
-    banci.emplace_back(10, "Maro");
-    banci.emplace_back(9, "Verzi");
-    banci.emplace_back(15, "Albastre");
+    banci.emplace_back(12, "Rosii", "buna", 123);
+    banci.emplace_back(10, "Maro", "proasta", 200);
+    banci.emplace_back(9, "Verzi", "medie", 195);
+    banci.emplace_back(15, "Albastre", "foarte buna", 230);
 
     Parc parc("IOR", 234, copaci, banci, lac);
     std::cout << parc;
 
-//    Copaci* sumC = Copaci::sumaC(copaci);
-//    sumC->afiscop();
-//    delete sumC;
-
+    int an;
+    std::cout << "Introduceti anul pana la care doriti sa aflati ce plante au fost puse: \n";
+    std::cin >> an;
+    copaci.cauta_plante(an);
     std::cout << "\n";
-    Banci* sumB = Banci::sumaB(banci);
-//    sumB->afisbnc();
-    delete sumB;
+
+    parc.achizitie_banci(2000);
+    std::cout << parc;
+
+//    std::cout << "\n";
+//    Banci* sumB = Banci::sumaB(banci);
+////    sumB->afisbnc();
+//    delete sumB;
 
     //Parc dim = dimensiune(parc);
     return 0;
